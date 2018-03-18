4adeaaec1f007ac4d43079cbd1c2c2171ab2f1e0
clement bacle <clement@officinasante.fr>
2018-03-18 16:09:50 +0100

    add homePage

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'pages/home'
+
   devise_for :users
   resources :profiles, only: [ :show, :new, :create, :edit, :update ]
   resources :thought_checks
   resources :tasks
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root to: 'pages#home'
 end
