055c5f9d84818f88766cac1f5b3392317d44efac
clement bacle <clement@officinasante.fr>
2018-03-20 20:50:43 +0100

    add controler profile / home and new / simple form for

--- Gemfile
+++ Gemfile
@@ -37,6 +37,11 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'devise'
 
+gem 'bootstrap-sass'
+gem 'autoprefixer-rails'
+gem 'simple_form'
+
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,19 +1,49 @@
 class ProfilesController < ApplicationController
+
+  before_action :set_profile, only: [ :show, :edit, :update, :destroy ]
+
   def show
+    redirect_to new_profile_path unless @profile
   end
 
   def new
+    @profile = Profile.new
   end
 
   def create
+    @user = User.new(current_user)
+    @profile = Profile.new(params_profile)
+    @profile.user = @user
+    if @profile.save
+      redirect_to profile_path(@user)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @profile.update(params_profile)
+    if @profile.save
+      redirect_to profile_path(@user)
+    else
+      render :new
+    end
   end
 
   def destroy
+    @profile.destroy
+  end
+
+  private
+
+  def params_profile
+    params.require(:profile).permit(:username, :age, :gender)
+  end
+
+  def set_user
+    @profile = User.profile(params[:id])
   end
 end
